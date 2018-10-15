529cdf63d64af9511de92dce529d91ff6d6d8d69
michael <42749620+heunek@users.noreply.github.com>
2018-10-15 20:58:00 +0200

    Update backlight.py

--- backlight.py
+++ backlight.py
@@ -1,11 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 import socket
+import rpi_backlight as bl
 
 UDP_IP = ''
 UDP_PORT = 50007
@@ -13,6 +7,13 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 while True:
     data, addr = sock.recvfrom(1024)
-    print("received message: ", data)
-    
-    
+    if data == b'display_on':
+        bl.set_power(True)
+    if data == b'display_off':
+        bl.set_power(False)
+    if data == b'display_low':
+        bl.set_brightness(20)
+    if data == b'display_medium':
+        bl.set_brightness(50)
+    if data == b'display_high':
+        bl.set_brightness(100)
