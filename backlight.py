07cd7b2d11af07f063bd39881cf27200965f9149
michael <42749620+heunek@users.noreply.github.com>
2020-08-06 22:55:33 +0200

    Update backlight.py
    Verwendung des rpi_backlight Moduls angepasst

--- backlight.py
+++ backlight.py
@@ -1,19 +1,23 @@
 import socket
-import rpi_backlight as bl
+from rpi_backlight import Backlight
+backlight =  Backlight()
 
 UDP_IP = ''
 UDP_PORT = 50007
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
+
+
+
 while True:
     data, addr = sock.recvfrom(1024)
     if data == b'display_on':
-        bl.set_power(True)
+        backlight.power = True
     if data == b'display_off':
-        bl.set_power(False)
+        backlight.power = False
     if data == b'display_low':
-        bl.set_brightness(20)
+        backlight.brightness = 10
     if data == b'display_medium':
-        bl.set_brightness(50)
+        backlight.brightness = 30
     if data == b'display_high':
-        bl.set_brightness(100)
+        backlight.brightness = 100
