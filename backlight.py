968663d78cfa45495b0887f58f2f07902dc45f53
michael <42749620+heunek@users.noreply.github.com>
2022-03-10 11:55:56 +0100

    Update backlight.py
    brightness level adjusted

--- backlight.py
+++ backlight.py
@@ -7,8 +7,6 @@ UDP_PORT = 50007
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
-
-
 while True:
     data, addr = sock.recvfrom(1024)
     if data == b'display_on':
@@ -16,8 +14,8 @@ while True:
     if data == b'display_off':
         backlight.power = False
     if data == b'display_low':
-        backlight.brightness = 10
+        backlight.brightness = 10 oder 25
     if data == b'display_medium':
-        backlight.brightness = 30
+        backlight.brightness = 30 oder 50
     if data == b'display_high':
-        backlight.brightness = 100
+        backlight.brightness = 100 oder 75
